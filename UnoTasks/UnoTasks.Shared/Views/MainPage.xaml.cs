9dfb78334c0c2462074ee00b258d19de1202798b
Elena Guzman <elena.guzbla@gmail.com>
2019-10-04 10:08:28 +0200

    :recycle: Create Views folder

--- UnoTasks/UnoTasks.Shared/Views/MainPage.xaml.cs
+++ UnoTasks/UnoTasks.Shared/Views/MainPage.xaml.cs
@@ -1,24 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using UnoTasks.Shared.Models;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using UnoTasks.Shared.Models;
 using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using static UnoTasks.Shared.Models.Item;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace UnoTasks
+namespace UnoTasks.Views
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
